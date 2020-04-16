d2aa69e2ceb42e0eac0b9d3cb811c3f8b63846bf
Enri Peters <epeters@schubergphilis.com>
2020-04-16 17:09:03 +0200

    Add _decktesting to .gitignore and added extra test

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -18,3 +21,15 @@ func TestNewDeck(t *testing.T) {
 	}
 }
 
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T)  {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 40 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+}
